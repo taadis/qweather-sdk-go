28b658f6ed49a31ef2ac06227bb48dbbdc642bb5
taadis <i@taadis.com>
2024-09-12 00:06:35 +0800

    fix:response refer when omitempty

--- client.go
+++ client.go
@@ -23,7 +23,7 @@ type baseRequest struct {
 
 type baseResponse struct {
 	Code  string `json:"code,omitempty"` // 状态码
-	Refer struct {
+	Refer *struct {
 		Sources []string `json:"sources,omitempty"` // 原始数据来源，或数据源说明，可能为空
 		License []string `json:"license,omitempty"` // 数据许可或版权声明，可能为空
 	} `json:"refer,omitempty"` //
