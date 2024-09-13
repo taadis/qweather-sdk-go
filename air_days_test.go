08db730ffbe749a9a4c5effae1bc407d10191347
taadis <i@taadis.com>
2024-09-14 00:11:57 +0800

    fix:index out of range [0]

--- air_days_test.go
+++ air_days_test.go
@@ -10,7 +10,7 @@ func TestClient_V7Air5Days(t *testing.T) {
 	req.Key = os.Getenv("QWETHER_KEY")
 	req.Location = "116.41,39.92"
 	req.Lang = "zh"
-	client := NewClient()
+	client := NewClient(WithLogEnabled(true))
 	resp, err := client.V7Air5Days(req)
 	if err != nil {
 		t.Fatalf("client.V7Air5Days error:%+v,", err)
@@ -18,14 +18,19 @@ func TestClient_V7Air5Days(t *testing.T) {
 
 	t.Logf("code:%s", resp.Code)
 	t.Logf("updateTime:%s", resp.UpdateTime)
-	daily := resp.Daily[0]
-	t.Logf("daily.fxDate:%s", daily.FxDate)
-	t.Logf("daily.aqi:%s", daily.AQI)
-	t.Logf("daily.level:%s", daily.Level)
-	t.Logf("daily.category:%s", daily.Category)
-	t.Logf("daily.primary:%s", daily.Primary)
-	refer := resp.Refer
-	t.Logf("refer.sources:%s", refer.Sources)
-	t.Logf("refer.license:%s", refer.License)
-	t.Logf("client.V7Air5Days resp:%s", resp.String())
+	if len(resp.Daily) > 0 {
+		daily := resp.Daily[0]
+		t.Logf("daily.fxDate:%s", daily.FxDate)
+		t.Logf("daily.aqi:%s", daily.AQI)
+		t.Logf("daily.level:%s", daily.Level)
+		t.Logf("daily.category:%s", daily.Category)
+		t.Logf("daily.primary:%s", daily.Primary)
+	}
+
+	if resp.Refer != nil {
+		refer := resp.Refer
+		t.Logf("refer.sources:%s", refer.Sources)
+		t.Logf("refer.license:%s", refer.License)
+		t.Logf("client.V7Air5Days resp:%s", resp.String())
+	}
 }
