626708b829cd601bbbbb69da5e9f050d0a5de94e
taadis <i@taadis.com>
2024-09-14 00:15:14 +0800

    fix:index out of range [0]

--- historical_air_test.go
+++ historical_air_test.go
@@ -17,21 +17,25 @@ func TestClient_V7HistoricalAir(t *testing.T) {
 		t.Fatalf("client.V7HistoricalAir error:%+v", err)
 	}
 
-	airHourly := resp.AirHourly[0]
-	refer := resp.Refer
 	t.Log("code:", resp.Code)
 	t.Log("fxLink:", resp.FxLink)
-	t.Log("airHourly.pubTime:", airHourly.PubTime)
-	t.Log("airHourly.aqi:", airHourly.AQI)
-	t.Log("airHourly.level:", airHourly.Level)
-	t.Log("airHourly.category:", airHourly.Category)
-	t.Log("airHourly.primary:", airHourly.Primary)
-	t.Log("airHourly.pm10:", airHourly.PM10)
-	t.Log("airHourly.pm2p5:", airHourly.PM2p5)
-	t.Log("airHourly.no2:", airHourly.NO2)
-	t.Log("airHourly.so2:", airHourly.SO2)
-	t.Log("airHourly.co:", airHourly.CO)
-	t.Log("airHourly.o3:", airHourly.O3)
-	t.Log("refer.sources:", refer.Sources)
-	t.Log("refer.license:", refer.License)
+	if resp.Refer != nil {
+		refer := resp.Refer
+		t.Log("refer.sources:", refer.Sources)
+		t.Log("refer.license:", refer.License)
+	}
+	if len(resp.AirHourly) > 0 {
+		airHourly := resp.AirHourly[0]
+		t.Log("airHourly.pubTime:", airHourly.PubTime)
+		t.Log("airHourly.aqi:", airHourly.AQI)
+		t.Log("airHourly.level:", airHourly.Level)
+		t.Log("airHourly.category:", airHourly.Category)
+		t.Log("airHourly.primary:", airHourly.Primary)
+		t.Log("airHourly.pm10:", airHourly.PM10)
+		t.Log("airHourly.pm2p5:", airHourly.PM2p5)
+		t.Log("airHourly.no2:", airHourly.NO2)
+		t.Log("airHourly.so2:", airHourly.SO2)
+		t.Log("airHourly.co:", airHourly.CO)
+		t.Log("airHourly.o3:", airHourly.O3)
+	}
 }
